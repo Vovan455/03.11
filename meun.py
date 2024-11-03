412ea245ec3c03aae05693c83f4fcf92b6c11e71
vova <ghdfkr@ffgg.com>
2024-11-03 13:32:01 +0200

    dfdfd

--- meun.py
+++ meun.py
@@ -1,1 +1,15 @@
-print ("привіт")
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+
+app = QApplication([])
+
+menu_window = QWidget()
+
+text = QLabel("Hello World")
+
+line = QVBoxLayout()
+line.addWidget(text)
+menu_window.setLayout(line)
+
+menu_window.show()
+
+app.exec()
---
+++ new.py
@@ -0,0 +1,9 @@
+print("hay")
+import sys 
+
+args = sys.argv
+print(args)
+
+second_arg = args[1]
+if second_arg == "1":
+    print("hello vova")
